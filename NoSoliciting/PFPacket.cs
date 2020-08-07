503ae950b09c4381b2936daacde99d96660f800e
Anna Clemens <git@annaclemens.io>
2020-08-07 18:08:24 -0400

    refactor: add more packet parsing

--- NoSoliciting/PFPacket.cs
+++ NoSoliciting/PFPacket.cs
@@ -11,7 +11,7 @@ namespace NoSoliciting {
     public struct PFPacket {
         private readonly int unk0;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
-        public byte[] padding1;
+        private readonly byte[] padding1;
 
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
         public PFListing[] listings;
@@ -19,8 +19,54 @@ namespace NoSoliciting {
 
     [StructLayout(LayoutKind.Sequential)]
     public struct PFListing {
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 128)]
-        public byte[] header;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 28)]
+        private readonly byte[] header1;
+
+        internal readonly ushort duty;
+        internal readonly ushort dutyType;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+        private readonly byte[] header2;
+
+        internal readonly ushort world;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        private readonly byte[] header3;
+
+        internal readonly byte objective;
+        internal readonly byte beginnersWelcome;
+        internal readonly byte conditions;
+        internal readonly byte dutyFinderSettings;
+        internal readonly byte lootRules;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        internal readonly byte[] header4; // all zero in every pf I've examined
+
+        internal readonly uint lastPatchHotfixTimestamp; // last time the servers were restarted?
+        internal readonly ushort secondsRemaining;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+        private readonly byte[] header5; // 00 00 01 00 00 00 in every pf I've examined
+
+        internal readonly ushort minimumItemLevel;
+        internal readonly ushort homeWorld;
+        internal readonly ushort currentWorld;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        private readonly byte[] header6; // 02 XX 01 00 in every pf I've examined
+
+        internal readonly byte searchArea;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
+        private readonly byte[] header7; // 00 01 00 00 00 for every pf except alliance raids where it's 01 03 00 00 00 (second byte # parties?)
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        internal readonly uint[] slots;
+        private readonly uint job; // job started as?
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        private readonly byte[] header8; // all zero in every pf I've examined
+
         // Note that ByValTStr will not work here because the strings are UTF-8 and there's only a CharSet for UTF-16 in C#.
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
         private readonly byte[] name;
@@ -32,12 +78,17 @@ namespace NoSoliciting {
             return Encoding.UTF8.GetString(nonNull);
         }
 
-        public string Name() {
+        internal string Name() {
             return HandleString(this.name);
         }
 
-        public string Description() {
+        internal string Description() {
             return HandleString(this.description);
+        }
+
+        internal bool IsNull() {
+            // a valid party finder must have at least one slot set
+            return this.slots.All(slot => slot == 0);
         }
     }
 }
