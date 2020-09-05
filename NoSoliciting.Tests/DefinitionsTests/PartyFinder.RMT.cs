29a73af5bc3c7f877d721040070e9b85bc69f020
Anna <git@anna.lgbt>
2020-09-05 13:04:18 -0400

    fix(defs): reduce false positives and negatives

--- NoSoliciting.Tests/DefinitionsTests/PartyFinder.RMT.cs
+++ NoSoliciting.Tests/DefinitionsTests/PartyFinder.RMT.cs
@@ -6,14 +6,6 @@ namespace NoSoliciting.Tests.DefinitionsTests.PartyFinder {
             this.Def = fixture.defs.PartyFinder["rmt"];
         }
 
-        public static object[][] DataNegatives => DefUtils.DataFromStrings(new string[] {
-            "Doing Art commission of your charactet with good price! more info add me on discord: d0uglaz#7409 ♥",
-            "Selling HQ 490 DoH/DoL sets, just in time for the Ishgard restoration project. Cheaper than MB, Whipser or join for info.",
-            "Looking to sell medium odder otter walls(2mil) join or tell.",
-            @"Selling 1x Eldthurs Horn for 8mil. Skip MB taxes \o/ Join if interested",
-            "Selling Phanta mats, let me know what you want and I can deliver it to you. 2k for 300k. Join the party don't /tell please",
-        });
-
         public static object[][] DataPositives => DefUtils.DataFromStrings(new string[] {
             "「Best Prices」《 Shiva Unreal ★ Warrior of Light ★ Ultimates ★ Eden's Verse i500/i505",
             "「 MINMAXØ 」 SALES ≪ ❶ Savage 一 ❷ Ultimates 一 ❸ Mounts Etc. ≫ World #1 teams, instant delivery. Discord → azrael#6447",
@@ -23,12 +15,21 @@ namespace NoSoliciting.Tests.DefinitionsTests.PartyFinder {
             "♥ SELLING ♥ Shiva Unreal / WoL XM - Eden Savage (☆ i500/i505 ☆) - TEA/UwU/UCoB - Old raids and + |  Discord add me: gin#5147",
         });
 
-        [Theory]
-        [MemberData(nameof(DataNegatives))]
-        public void Negatives(string msg) => this.Check(msg, CheckType.Negative);
+        public static object[][] DataNegatives => DefUtils.DataFromStrings(new string[] {
+            "Doing Art commission of your charactet with good price! more info add me on discord: d0uglaz#7409 ♥",
+            "Selling HQ 490 DoH/DoL sets, just in time for the Ishgard restoration project. Cheaper than MB, Whipser or join for info.",
+            "Looking to sell medium odder otter walls(2mil) join or tell.",
+            @"Selling 1x Eldthurs Horn for 8mil. Skip MB taxes \o/ Join if interested",
+            "Selling Phanta mats, let me know what you want and I can deliver it to you. 2k for 300k. Join the party don't /tell please",
+            "#1 NA WHOLESALER! NEO SET-660K! i490 SET-1.5M(w/DISCOUNT OPTIONS)! BUYING PHANTAS ANY AMOUNT 3K! JOIN PARTY!!!",
+        });
 
         [Theory]
         [MemberData(nameof(DataPositives))]
         public void Positives(string msg) => this.Check(msg, CheckType.Positive);
+
+        [Theory]
+        [MemberData(nameof(DataNegatives))]
+        public void Negatives(string msg) => this.Check(msg, CheckType.Negative);
     }
 }
