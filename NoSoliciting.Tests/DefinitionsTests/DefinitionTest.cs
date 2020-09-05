2472a81ce8a25bd1c544804ada2c827bba2e5a18
Anna Clemens <git@annaclemens.io>
2020-09-05 14:30:58 -0400

    feat(tests): add more global negatives

--- NoSoliciting.Tests/DefinitionsTests/DefinitionTest.cs
+++ NoSoliciting.Tests/DefinitionsTests/DefinitionTest.cs
@@ -15,11 +15,15 @@ namespace NoSoliciting.Tests.DefinitionsTests {
             new TestMessage("T/N H/S Tethers DPS Towers KB prevention dps uptime, Partners for the rest no salt farm party"),
             new TestMessage("another day another camp on the finder! u know the drill by now bahaprog or nael clean up "),
             new TestMessage("♡Need help with anything?♥ Chat, hangout, and can craft! Levekits, gear, & more!☂ Pop in, take a seat, & maybe I can help! :)"),
+            new TestMessage("Unsync clear for my alt. Come for WT, bonus poetics, or out of the goodness of your heart."),
 
             // messages
             new TestMessage(ChatType.FreeCompany, "forgot to leave a commendation, I forget this sometimes"),
             new TestMessage(ChatType.StandardEmote, "Anri Valmet laughs at Kyoya Kazuki."),
             new TestMessage(ChatType.Shout, "Need a lvl 80 crafter? Buy a leve kit and get your crafter today! PST for price info :D"),
+            new TestMessage(ChatType.Yell, "Wedding starting at 3:00pm est, pst for an invite. You will get pets. pst"),
+            new TestMessage(ChatType.Shout, "LOCAL ShB Train! Spend your nuts & seals. Let’s go kill things and be savages. This hunt is brought to you by ★VIII - The Double Infinity Free Company! ♥∞"),
+            new TestMessage(ChatType.Shout, "☆☆Shadowbringers Hunt Train • A Rank☆☆ Starting at  《Instance》  → Lakeland ( 27.6  , 15.3 )"),
         });
 
         [Theory]
