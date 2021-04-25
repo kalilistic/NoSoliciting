15c1aff05a89f671476bce9b14ec42f517c556fc
Anna Clemens <git@annaclemens.io>
2021-04-25 11:42:50 -0400

    feat: add checksum validation for models
    Also truncate new model zips before writing, fixing a long-standing issue.

--- NoSoliciting/Ml/Manifest.cs
+++ NoSoliciting/Ml/Manifest.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 
 namespace NoSoliciting.Ml {
+    [Serializable]
     public class Manifest {
         public uint Version { get; set; }
         public Uri ModelUrl { get; set; }
+        public string ModelHash { get; set; }
         public Uri ReportUrl { get; set; }
+
+        public byte[] Hash() => Convert.FromBase64String(this.ModelHash);
     }
 }
