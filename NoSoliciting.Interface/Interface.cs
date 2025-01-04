fc68ff8204c05594917f5403662e7a375ff9bde2
kalilistic <35899782+kalilistic@users.noreply.github.com>
2025-01-04 16:24:58 -0500

    refactor: remove interface and trainer from plugin source

--- NoSoliciting.Interface/Interface.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace NoSoliciting.Interface {
-    public interface IClassifier : IDisposable {
-        void Initialise(byte[] data);
-
-        string Classify(ushort channel, string message);
-    }
-}
